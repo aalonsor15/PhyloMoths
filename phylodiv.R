eeb841b2cacfa98bbddb9048b67582124769034e
Aura M. Alonso-Rodriguez <aura.alonso-rodriguez@uvm.edu>
2022-05-26 12:59:58 -0400

    initial commit - starting the R script

---
+++ phylodiv.R
@@ -0,0 +1,6 @@
+# ----------------------------------------------------------------------
+# El Verde moths project: Calculating phylogenetic diversity metrics
+# 26 May 2022
+# Aura M. Alonso-Rodríguez
+# ----------------------------------------------------------------------
+#
